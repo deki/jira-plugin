e07cae5a51db91b61cdfb7f1e69bd8f3b07751fe
Dennis Kieselhorst <mail@dekies.de>
2016-01-08 11:59:39 +0100

    increase test coverage

--- src/test/java/hudson/plugins/jira/JiraVersionCreatorTest.java
+++ src/test/java/hudson/plugins/jira/JiraVersionCreatorTest.java
@@ -5,6 +5,8 @@ import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.model.Result;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -13,6 +15,7 @@ import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -79,14 +82,21 @@ public class JiraVersionCreatorTest {
     }
 
     @Test
+    public void jiraApiCalledWithInvalidParameters() throws InterruptedException, IOException {
+        jvc.setJiraProjectKey("");
+        jvc.setJiraVersion("");
+        when(listener.fatalError(Mockito.anyString(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(mock(PrintWriter.class));
+        boolean result = jvc.perform(build, launcher, listener);
+        verify(listener).finished(Result.FAILURE);
+        assertThat(result, is(false));
+    }
+
+    @Test
     public void buildDidNotFailWhenVersionExists() throws IOException, InterruptedException {
         Set<JiraVersion> existingVersions = new HashSet<JiraVersion>();
         existingVersions.add(new JiraVersion(JIRA_VER, null, false, false));
 
         when(site.getVersions(JIRA_PRJ)).thenReturn(existingVersions);
-
-        PrintStream logger = mock(PrintStream.class);
-        when(listener.getLogger()).thenReturn(logger);
 
         boolean result = jvc.perform(build, launcher, listener);
         verify(site, times(0)).addVersion(JIRA_VER, JIRA_PRJ);
@@ -107,9 +117,6 @@ public class JiraVersionCreatorTest {
         existingVersions.add(new JiraVersion(JIRA_VER, null, false, false));
 
         when(site.getVersions(JIRA_PRJ)).thenReturn(existingVersions);
-
-        PrintStream logger = mock(PrintStream.class);
-        when(listener.getLogger()).thenReturn(logger);
 
         boolean result = jvc.perform(build, launcher, listener);
         verify(site, times(0)).addVersion(JIRA_VER, JIRA_PRJ);
